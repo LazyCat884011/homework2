ca09a017773dd7af03604cf633c6b396c0d018ea
WINDOWS-3OV9AAS\User <tommy2007321@yahoo.com.tw>
2018-04-26 22:57:14 +0800

    補上註解 且改成10組

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,34 +10,48 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            
+            // 宣告亂數
             Random rand = new Random();
+
+            // 宣告執行次數n
             int n = 0;
             
-            while (n<=20)
+            // 創建迴圈
+            while (n<10)
             {
                 
 
-                Console.Clear();
-                Console.WriteLine("╔═══════════════════════════╗");
+             // 美化   
+                Console.WriteLine("╔══════════════════════════════════════════════════════╗");
                 Console.WriteLine("║               大         樂         透               ║");
-                Console.WriteLine("╠═══╦═══╦═══╦═══╦═══╦═══╦═══╣");
-                
+                Console.WriteLine("╠══════╦═══════╦═══════╦═══════╦═══════╦═══════╦═══════╣");
+             
+             // 宣告6個從1~43取出的亂數   
                 int r1 = rand.Next(1, 43);
                 int r2 = rand.Next(1, 43);
                 int r3 = rand.Next(1, 43);
                 int r4 = rand.Next(1, 43);
                 int r5 = rand.Next(1, 43);
                 int r6 = rand.Next(1, 43);
-                
+             
+             // 把字改紅色   
                 Console.ForegroundColor = ConsoleColor.Red;
 
-                Console.WriteLine("║【FN】║  {1:00}  ║  {2:00}  ║  {3:00}  ║  {4:00}  ║  {5:00}  ║  {6:00}  ║", n,r1, r2, r3, r4, r5, r6);
+             // 印出亂數
+                Console.WriteLine("║【FN】║  {1:00}   ║  {2:00}   ║  {3:00}   ║  {4:00}   ║  {5:00}   ║  {6:00}   ║", n,r1, r2, r3, r4, r5, r6);
+
+             // 執行完後迴圈次數n+1
                 n++;
-                Console.WriteLine("╚═══╩═══╩═══╩═══╩═══╩═══╩═══╝");
-                System.Threading.Thread.Sleep(50);
+
+             // 美化
+                Console.WriteLine("╚══════╩═══════╩═══════╩═══════╩═══════╩═══════╩═══════╝");
+                
                 
 
             }
+
+            // 結束
             Console.ReadLine();
 
             
